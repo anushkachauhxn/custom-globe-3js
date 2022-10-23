7362c3807194971a3a7b194ec32ced37b724b5dd
Anushka Chauhan <anushkachauhan1503@gmail.com>
2022-10-23 18:54:02 +0530

    Add sphere mesh

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,12 @@
 import * as THREE from "three";
 
+// Renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(innerWidth, innerHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+document.body.appendChild(renderer.domElement);
+
+// Scene + Camera
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -7,13 +14,20 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
+camera.position.set(0, 0, 10);
 
-const renderer = new THREE.WebGL1Renderer();
-renderer.setSize(innerWidth, innerHeight);
-document.body.appendChild(renderer.domElement);
+// Texture Loader
+const loader = new THREE.TextureLoader();
 
-function animate() {
+// Add Objects
+// 1. Sphere
+const sphereGeo = new THREE.SphereGeometry(5, 50, 50);
+const sphereMat = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const sphere = new THREE.Mesh(sphereGeo, sphereMat);
+scene.add(sphere);
+
+// Animate
+(function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
-}
-animate();
+})();
